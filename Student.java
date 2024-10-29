ad2d516d18934f4bb47ac827936a4040ddbebc97
JosefBvo <154392935+JosefBvo@users.noreply.github.com>
2024-10-29 11:37:40 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,5 +9,44 @@ package com.mycompany.studentmanagementsystem;
  * @author josef
  */
 public class Student {
+    private int StudentID;
+    private String StudentName;
+    private double StudentGrade;
+    
+    /**
+     *
+     * @param StudentID
+     * @param StudentName
+     * @param StudentGrade
+     */
+    //constructor begins
+    public Student(int StudentID, String StudentName, double StudentGrade){
+        StudentID = StudentID;
+        StudentName = StudentName;
+        StudentGrade = StudentGrade;
+    }       
+    //Accessors!!!
+    public int getStudentID(){
+        return StudentID;
+    }
+    public String getStudentName(){
+        return StudentName;
+    }   
+    public double getStudentGrade(){
+        return StudentGrade;
+    }
+    
+    //Mutators!!!
+    public void setStudentID(int StudentID){
+        this.StudentID = StudentID;   
+    }
+    
+    public void setStudentName(String StudentName){
+        this.StudentName = StudentName;
+    }
+    
+    public void setStudentGrade(int StudentGrade){
+        this.StudentGrade = StudentGrade;
+    }
     
 }
