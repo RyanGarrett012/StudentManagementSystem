5b71846a5fd4d92512ab9109c5198029926e4fef
JosefBvo <154392935+JosefBvo@users.noreply.github.com>
2024-10-29 11:43:00 -0500

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -9,5 +9,30 @@ package com.mycompany.studentmanagementsystem;
  * @author josef
  */
 public class Subject {
+    private String SubjectName;
+    private double SubjectGrade;
+    //Constructor begins
+    public Subject(String SubjectName, double SubjectGrade){
+        SubjectName = SubjectName;
+        SubjectGrade = SubjectGrade;
+                
+    }
+    //Accessors!!!
+    public String getSubjectName(){
+        return SubjectName;
+    }
     
+    public double getSubjectGrade(){
+        return SubjectGrade;
+    }
+    //Mutatators!!!
+    public void setSubjectName(){
+        this.SubjectName = SubjectName;
+    }
+    
+    public void setSubjectGrade(){
+        this.SubjectGrade = SubjectGrade;
+    }
 }
+
+
