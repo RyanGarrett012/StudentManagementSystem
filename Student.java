8b4cb46843a92dd684d8f0c38aa9928a21b5f424
JosefBvo <154392935+JosefBvo@users.noreply.github.com>
2024-11-04 22:57:39 -0600

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,63 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.studentmanagementsystem;
-
+package com.mycompany.mavenproject1;
 /**
  *
- * @author josef
+ * @author Josef's PC
  */
 import java.util.HashMap;
 import java.util.Map;
 
 public class Student {
-    private String StudentName; 
-    private int id; 
-    private Map<Subject, Integer> grades; 
-
-    // Constructor!!!!
-    public Student(String name, int id) {
+    private String StudentName;
+    private int id;
+    private Map<Subject, Integer> grades;
+    //Constructor
+    public Student(String StudentName, int id) {
         this.StudentName = StudentName;
         this.id = id;
-        this.grades = new HashMap<>(); 
+        this.grades = new HashMap<>();
     }
-
-    //Accessors!!!!
+    //Accessors!!!
     public String getStudentName() {
         return StudentName;
     }
-    
+
     public int getId() {
         return id;
     }
-    //Method that enrolls students into a subject
+    //Method to assign a SUBJECT to a given Student
     public void enrollInSubject(Subject subject) {
         if (!grades.containsKey(subject)) {
-            grades.put(subject, null); 
+            grades.put(subject, null);
         }
     }
-    // Method to add or update a grade for a specific subject
+    //Can give a grade to a student for a specific Subject
     public void addGrade(Subject subject, int grade) {
         if (grades.containsKey(subject)) {
-            grades.put(subject, grade); 
-        } else { //input validation
-            System.out.println("Student not enrolled in " + subject.getName());
+            grades.put(subject, grade);
+        } else {
+            System.out.println("Student not enrolled in " + subject.getSubjectName());
         }
     }
-    //Accessor for a specific grade
+    //Accessor for a specific subject grade, it is special
     public Integer getGrade(Subject subject) {
-        return grades.get(subject); 
+        return grades.get(subject);
     }
     //Accessor for all grades
     public Map<Subject, Integer> getGrades() {
-        return grades; 
+        return grades;
     }
-
-    // Ensures a string is returned
+    //ensures a string is returned
     @Override
     public String toString() {
-        return "Student{" + "name='" + StudentName + '\'' + ", id=" + id + '}';
+        return "Student{" + "StudentName='" + StudentName + '\'' + ", id=" + id + '}';
     }
 }
-
