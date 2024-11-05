66e8874db1d583c8d3e04a04d2cca6d24a636e6f
JosefBvo <154392935+JosefBvo@users.noreply.github.com>
2024-11-04 22:58:01 -0600

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,29 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.studentmanagementsystem;
+package com.mycompany.mavenproject1;
 
 /**
  *
- * @author josef
+ * @author Josef's PC
  */
 public class Subject {
     private String SubjectName;
-
-    //constructor!!!
-    public Subject(String name) {
+    //Constructor
+    public Subject(String SubjectName) {
         this.SubjectName = SubjectName;
     }
     //Accessor
     public String getSubjectName() {
         return SubjectName;
     }
-
-    @Override //so that subjects are always referenced as a string
+    //Ensures a string is returned
+    @Override
     public String toString() {
         return SubjectName;
     }
 }
-
 
